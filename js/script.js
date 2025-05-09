37b600ff15322a0a0c1dca67dd2d07b33dae5706
maxhenschel-US <maxhenschel@alum.calarts.edu>
2025-05-09 12:12:51 -0400

    made updates to style and content across site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Max Henschel - GitHub: maxhenschel-us - Email: maxhenschel@alum.calarts.edu");
+// Console log info
+console.log("Name: Max Henschel");
+console.log("GitHub: https://github.com/maxhenschel-us");
+console.log("Email: maxhenschel@gmail.com");
+console.log("Artist: fashi0ncrime$");
+
+// Fade-in effect on scroll
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle('visible', entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.25,
+  }
+);
+
+sections.forEach(section => observer.observe(section));
